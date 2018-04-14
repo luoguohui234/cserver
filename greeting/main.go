0a83719e2ca9e32e183feb841d916e812d58e0f4
luoguohui234 <luoguohui1986@gmail.com>
2018-04-14 09:14:16 -0700

    Init file

---
+++ greeting/main.go
@@ -0,0 +1,22 @@
+
+package greeting
+
+import (
+	"io"
+	"net/http"
+	"log"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
